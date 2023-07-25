71d6d8088b452c7292d1f5175742dc8444b350c0
Suyash Kadam <suyash.kadam01@ad.infosys.com>
2023-07-26 04:16:40 +0530

    Saving Account (Max Withdraw Limit)

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -3,14 +3,12 @@ package com.driver;
 public class SavingsAccount extends BankAccount{
     double rate;
     double maxWithdrawalLimit;
-    double totalWithdraw;
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
         super(name, balance, 0);
         this.rate = rate;
         this.maxWithdrawalLimit = maxWithdrawalLimit;
-        this.totalWithdraw = 0;
     }
 
     public double getRate() {
@@ -21,15 +19,11 @@ public class SavingsAccount extends BankAccount{
         return maxWithdrawalLimit;
     }
 
-    public double getTotalWithdraw() {
-        return totalWithdraw;
-    }
-
     public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
-        if(totalWithdraw + amount > maxWithdrawalLimit)
+        if(amount > maxWithdrawalLimit)
         {
             throw new Exception("Maximum Withdraw Limit Exceed");
         }
